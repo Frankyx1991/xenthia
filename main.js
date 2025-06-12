4f300695561f1a87a91846852cb913b70977c47a
Frankyx1991 <131958713+Frankyx1991@users.noreply.github.com>
2025-06-13 00:04:11 +0200

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-
-require('dotenv').config();
-const cerebroAuto = require('./cerebro_automejora');
-
-// Ejecutar revisión de Cerebro IA cada 6 horas
-setInterval(async () => {
-  console.log("🧠 Revisando Cerebro IA...");
-  await cerebroAuto.ejecutarRevision();
-}, 6 * 60 * 60 * 1000); // 6 horas
