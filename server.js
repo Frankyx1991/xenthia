b42907820960c1d004a1d56e811e25c10440a943
Frankyx1991 <131958713+Frankyx1991@users.noreply.github.com>
2025-06-13 00:04:50 +0200

    Delete server.js

--- server.js
+++
@@ -1,4 +0,0 @@
-const express = require('express');
-const app = express();
-app.get('/', (req, res) => res.send('Xenthia activo'));
-app.listen(3000, () => console.log('Servidor activo en puerto 3000'));
